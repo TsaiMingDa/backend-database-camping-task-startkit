f5b02085d2bcd8a39ae13b0f67f0c6bbc3c4ab94
TsaiMingDa <ss95159@gmail.com>
2024-12-02 21:54:33 +0800

    完成6-1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -245,7 +245,12 @@ group by user_id;
 -- 6. 後台報表
 -- 6-1 查詢：查詢專長為重訓的教練，並按經驗年數排序，由資深到資淺（需使用 inner join 與 order by 語法)
 -- 顯示須包含以下欄位： 教練名稱 , 經驗年數, 專長名稱
-
+select "USER".name, "COACH".experience_years, "SKILL"."name"  from "COACH_LINK_SKILL"
+inner join "COACH" on "COACH".id = "COACH_LINK_SKILL".coach_id
+inner join "USER" on "USER".id = "COACH".user_id
+inner join "SKILL" on "SKILL".id = "COACH_LINK_SKILL".skill_id
+where "SKILL"."name" = '重訓'
+order by "COACH".experience_years desc;
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
 

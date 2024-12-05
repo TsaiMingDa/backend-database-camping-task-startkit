34384dc860d15bbe6f14caeef384cfdda7d4a0bb
TsaiMingDa <ss95159@gmail.com>
2024-12-05 23:51:32 +0800

    完成5-8

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -234,7 +234,29 @@ group by user_id;
     -- from ( 用戶王小明的購買堂數 ) as "CREDIT_PURCHASE"
     -- inner join ( 用戶王小明的已使用堂數) as "COURSE_BOOKING"
     -- on "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
-
+select 
+	"CREDIT_PURCHASE".user_id,
+	("CREDIT_PURCHASE".total_credit - "COURSE_BOOKING".used_credit) as remaining_credit
+from
+	( --用戶王小明的購買堂數
+		select 
+			user_id, 
+			sum(purchased_credits) as total_credit 
+		from "CREDIT_PURCHASE"
+		where user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io')
+		group by user_id
+	) as "CREDIT_PURCHASE"
+inner join (
+    -- 用戶王小明的已使用堂數
+	select
+		user_id,
+		count(*) as used_credit 
+	from "COURSE_BOOKING"
+	where user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io')
+	and status != '已取消'
+	GROUP BY user_id
+) as "COURSE_BOOKING"
+on "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
 
 -- ████████  █████   █     ███  
 --   █ █   ██    █  █     █     
